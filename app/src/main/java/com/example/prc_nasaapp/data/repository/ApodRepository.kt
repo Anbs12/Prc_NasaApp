23b57d73f3d84e0160540d14fc4aeeb33a601095
Anbs12 <118308826+Anbs12@users.noreply.github.com>
2025-04-24 13:25:05 -0400

    refactor: Update repository and dependencies
    - Add a comment to `ApodRepository` indicating dependency injection.
    - Update `composeBom` dependency to version `2025.04.01` in `libs.versions.toml`.

--- app/src/main/java/com/example/prc_nasaapp/data/repository/ApodRepository.kt
+++ app/src/main/java/com/example/prc_nasaapp/data/repository/ApodRepository.kt
@@ -2,7 +2,7 @@ package com.example.prc_nasaapp.data.repository
 
 import com.example.prc_nasaapp.data.network.NasaApiService
 
-//TODO hacer uso de Koin.
+/**Repository inyeccion de dependencia.*/
 class ApodRepository(
     private val api: NasaApiService
 ){

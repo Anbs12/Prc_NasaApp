b0a67dfe05ee4d25581db641796d415820258103
Anbs12 <118308826+Anbs12@users.noreply.github.com>
2025-06-03 20:29:29 -0400

    refactor: Remove unused imports from SelectionScreen.kt
    - Remove unused `Configuration` and `Preview` imports.

--- app/src/main/java/com/example/prc_nasaapp/ui/screens/selectionScreen/SelectionScreen.kt
+++ app/src/main/java/com/example/prc_nasaapp/ui/screens/selectionScreen/SelectionScreen.kt
@@ -1,6 +1,5 @@
 package com.example.prc_nasaapp.ui.screens.selectionScreen
 
-import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -21,7 +20,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.prc_nasaapp.R
